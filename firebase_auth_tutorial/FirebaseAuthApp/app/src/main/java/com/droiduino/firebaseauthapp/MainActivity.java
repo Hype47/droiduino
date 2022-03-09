b2bc1d732f40617aee96079687100fdf31a53370
Hype47 <12470584+Hype47@users.noreply.github.com>
2022-03-09 21:15:36 +0700

    Automatic Log Out

--- firebase_auth_tutorial/FirebaseAuthApp/app/src/main/java/com/droiduino/firebaseauthapp/MainActivity.java
+++ firebase_auth_tutorial/FirebaseAuthApp/app/src/main/java/com/droiduino/firebaseauthapp/MainActivity.java
@@ -1,8 +1,23 @@
 package com.droiduino.firebaseauthapp;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +25,44 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        // Build a GoogleSignInClient with the options specified by gso.
+        GoogleSignInClient mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        // Automatically log-out when screen is refreshed
+        FirebaseAuth.getInstance().signOut();
+        mGoogleSignInClient.signOut();
+
+        // Sign in Activity
+        TextView statusText = findViewById(R.id.textStatus);
+        ActivityResultLauncher<Intent> activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if(result.getResultCode() == Activity.RESULT_OK){
+                        // System Message
+                        Log.d("signIn", "sign-in successful");
+                        statusText.setText("Log in successful");
+                    }
+                }
+            });
+
+        // Sign In Button
+        Button signInButton = findViewById(R.id.btnSignIn);
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+                activityResultLauncher.launch(signInIntent);
+            }
+        });
+
+
     }
 }
